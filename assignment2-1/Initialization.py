2c35be8dd0c4933a1a4bfdb3e61f4c066e7ffcec
Xiang Zhang <nanfang147@yeah.net>
2018-07-17 19:50:28 +0800

    update

---
+++ assignment2-1/Initialization.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+train_X, train_Y, test_X, test_Y = load_dataset()
+
+
+def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True, initialization = "he"):
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    layers_dims = [X.shape[0], 10, 5, 1]
+
+    # Initialize parameters dictionary
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+
+    for i in range(num_iterations):
+        A3, cache = forward_propagation(X, parameters)
+
+        cost = compute_loss(A3, Y)
+
+        grads = backward_propagation(X, Y, cache)
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+
+def initialize_parameters_zeros(layers_dims):
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters["W" + str(l)] = np.zeros((layers_dims[l], layers_dims[l-1]))
+        parameters["b" + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+## test code
+# parameters = initialize_parameters_zeros([3,2,1])
+# print("W1 = " + str(parameters["W1"]))
+# print("b1 = " + str(parameters["b1"]))
+# print("W2 = " + str(parameters["W2"]))
+# print("b2 = " + str(parameters["b2"]))
+# parameters = model(train_X, train_Y, initialization = "zeros")
+# print ("On the train set:")
+# predictions_train = predict(train_X, train_Y, parameters)
+# print ("On the test set:")
+# predictions_test = predict(test_X, test_Y, parameters)
+
+
+def initialize_parameters_random(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * 10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+### test code
+# parameters = initialize_parameters_random([3, 2, 1])
+# print("W1 = " + str(parameters["W1"]))
+# print("b1 = " + str(parameters["b1"]))
+# print("W2 = " + str(parameters["W2"]))
+# print("b2 = " + str(parameters["b2"]))
+# parameters = model(train_X, train_Y, initialization = "random")
+# print ("On the train set:")
+# predictions_train = predict(train_X, train_Y, parameters)
+# print ("On the test set:")
+# predictions_test = predict(test_X, test_Y, parameters)
+
+
+def initialize_parameters_he(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1
+     
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * np.sqrt(2.0/layers_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        
+    return parameters
+
+### test code
+# parameters = initialize_parameters_he([2, 4, 1])
+# print("W1 = " + str(parameters["W1"]))
+# print("b1 = " + str(parameters["b1"]))
+# print("W2 = " + str(parameters["W2"]))
+# print("b2 = " + str(parameters["b2"]))
+# parameters = model(train_X, train_Y, initialization = "he")
+# print ("On the train set:")
+# predictions_train = predict(train_X, train_Y, parameters)
+# print ("On the test set:")
+# predictions_test = predict(test_X, test_Y, parameters)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
